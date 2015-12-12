853453746d859579c3761e9604674f167f44f34e
Nick Weedon n2798271 <nick@weedon.org.au>
2015-12-12 15:01:10 +1000

    Fixed incorrect namespace in comments

--- src/amd.js
+++ src/amd.js
@@ -4,13 +4,13 @@ define('xhr-adaptor-bq-js',
     function (xhrAdaptorJs) {
 
 		/**
-		 * For the AMD module version of the library, the xhrBQJs namespace
+		 * For the AMD module version of the library, the xhrAdaptorJs namespace
 		 * does not exist but refers instead to the AMD module itself.
 		 *
 		 * @summary The xhrAdaptorJs namespace and AMD module
 		 * @version 1.0
 		 * @exports xhr-bq-adaptor-js
-		 * @namespace {object} xhrBQJs
+		 * @namespace {object} xhrAdaptorJs
 		 */
 		xhrAdaptorJs = xhrAdaptorJs || {};
 
